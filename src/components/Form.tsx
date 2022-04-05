806ece5083143532d6d333002abe1b4f46525f87
siddnikh <mailtosiddnikh@gmail.com>
2022-04-05 14:22:03 +0530

    Added onchange to input fields

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -11,12 +11,14 @@ const formfields = [
 export default function Form(props: {closeFormCB : () => void}){
     
     const [formState, setFormState] = useState(formfields); //the initial state is the formfields
+    const [fieldState, setFieldState] = useState("");
+
     const addFormField = () => {
         setFormState([
             ...formState,
             {
                 id: Number(new Date()),
-                label: "New Field",
+                label: fieldState,
                 type: "text"
 
             },
@@ -30,31 +32,44 @@ export default function Form(props: {closeFormCB : () => void}){
     return(
         <div>
         {formState.map((field) => (
-            <div>
+            <div className="flex">
                 <div className="flex-1" key={field.id}>
-                <label>{field.label}</label>
-                <input
-                className="border-2 border-gray-200 rounded-lg p-2 m-2 w-full"
-                type={field.type}
-                />
+                    <label>{field.label}</label>
+                    <input
+                    className="border-2 border-gray-200 rounded-lg p-2 m-2 w-full"
+                    type={field.type}
+                    />
                 </div>
                 <div>
                     <button 
-                    className="flex-1 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 m-2 rounded"
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 ml-6 mt-9 rounded"
                     onClick={() => removeFormField(field.id)}
                     >Remove</button>
                 </div>
             </div>
           ))}
+          <div className='flex'>
+            <div className="flex-1">
+                <input
+                className="border-2 border-gray-200 rounded-lg p-2 m-2 w-full"
+                type="text"
+                onChange={(e) => {
+                    setFieldState(e.target.value);
+                }}
+                />
+            </div>
+            <div>
+                <button 
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 ml-6 mt-3 rounded"
+                onClick={() => addFormField()}
+                >Add Field</button>
+            </div>
+            </div>
           <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 m-2 rounded" type="submit">Submit</button>
           <button 
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 m-2 rounded" 
           onClick={props.closeFormCB}
           >Close Form</button>
-          <button 
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 m-2 rounded" 
-          onClick={() => addFormField()}
-          >Add Field</button>
           </div>
         );
 }
