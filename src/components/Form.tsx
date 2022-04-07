85819f678eb7ab79fe2cb7ee611cf327d0e453e8
siddnikh <mailtosiddnikh@gmail.com>
2022-04-07 13:20:19 +0530

    Fixed issues

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,16 +1,32 @@
 import React, { useState } from 'react';
 
-const formfields = [
-    {id: 1, label: "First Name", type: "text"},
-    {id: 2, label: "Last Name", type: "text"},
-    {id: 3, label: "Email", type: "email"},
-    {id: 4, label: "Date of Birth", type: "date"},
-    {id: 5, label: "Phone Number", type: "tel"},
-  ]
+interface formField {
+    id: number,
+    label: string,
+    type: string,
+    value: string
+}
+
+const initialformfields = [
+    {id: 1, label: "First Name", type: "text", value: ""},
+    {id: 2, label: "Last Name", type: "text", value: ""},
+    {id: 3, label: "Email", type: "email", value: ""},
+    {id: 4, label: "Date of Birth", type: "date", value: ""},
+    {id: 5, label: "Phone Number", type: "tel", value: ""},
+  ]  
 
 export default function Form(props: {closeFormCB : () => void}){
-    
-    const [formState, setFormState] = useState(formfields); //the initial state is the formfields
+
+    const initialState : () => formField[] = () => {
+        const formFieldsJSON = localStorage.getItem("formFields");
+        const formFieldsString = formFieldsJSON ? 
+        JSON.parse(formFieldsJSON) :
+        initialformfields;
+
+        return formFieldsString;
+    }
+
+    const [formState, setFormState] = useState(initialState()); //the initial state is the formfields
     const [newFieldState, setNewField] = useState("");
 
     const addFormField = () => {
@@ -19,17 +35,40 @@ export default function Form(props: {closeFormCB : () => void}){
             {
                 id: Number(new Date()),
                 label: newFieldState,
-                type: "text"
+                type: "text",
+                value: "",
 
             },
         ]);
         setNewField("");
     };
 
+    const updateField = (value: string, id: number) => {
+        setFormState(
+            formState.map((field) => {
+                if(field.id === id) {
+                    return {...field, value: value};
+                }
+                return field;
+            })
+        );
+        console.log(formState);
+    };
+
     const removeFormField = (id: number) => {
         setFormState(formState.filter((field) => field.id !== id));
     }
     
+    const saveFormData = (currentState: formField[]) => {
+        localStorage.setItem("formFields", JSON.stringify(currentState));
+    }
+
+    const clearFormData = () => {
+        setFormState(
+            formState.map((field) => {return {...field, value:""}})
+        )
+    }
+
     return(
         <div>
         {formState.map((field) => (
@@ -39,6 +78,10 @@ export default function Form(props: {closeFormCB : () => void}){
                     <input
                     className="border-2 border-gray-200 rounded-lg p-2 m-2 w-full"
                     type={field.type}
+                    value={field.value}
+                    onChange={(e) => {
+                        updateField(e.target.value, field.id);
+                    }}
                     />
                 </div>
                 <div>
@@ -67,11 +110,18 @@ export default function Form(props: {closeFormCB : () => void}){
                 >Add Field</button>
             </div>
             </div>
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 m-2 rounded" type="submit">Submit</button>
+          <button 
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 m-2 rounded"
+          onClick={() => saveFormData(formState)}
+          >Save</button>
           <button 
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 m-2 rounded" 
           onClick={props.closeFormCB}
           >Close Form</button>
+          <button 
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 m-2 rounded" 
+          onClick={() => clearFormData()}
+          >Clear Form</button>
           </div>
         );
 }
