484e77001353965ea3e1aab6db51a1c7ab42ccdc
siddnikh <mailtosiddnikh@gmail.com>
2022-04-27 23:28:25 +0530

    Integrated Sentry

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+
+Sentry.init({
+  dsn: "https://3bc178945cee46858eddf4c4db00a189@o1222564.ingest.sentry.io/6366537",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <App/>,
