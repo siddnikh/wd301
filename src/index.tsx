a5abb44c5ea291ef568afee8e28d323245da9e3a
siddnikh <mailtosiddnikh@gmail.com>
2022-04-27 23:16:21 +0530

    Added PWA support

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <App/>,
   document.getElementById('root')
 );
+
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
