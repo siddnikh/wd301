cdfcb52c5d153355651526239e3b2d2ee75efc26
siddnikh <mailtosiddnikh@gmail.com>
2022-04-27 23:20:33 +0530

    Added Lazy Loading

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,19 +1,21 @@
+import React from 'react';
 import { useRoutes } from 'raviger';
-import Home from '../components/Home';
-import Form from '../components/Form';
-import FormList from '../components/FormList';
 //import Preview from '../components/Preview';
 import Login from '../components/Login';
 import Logout from '../components/Logout';
 
+const Home = React.lazy(() => import("../components/Home"));
+const Form = React.lazy(() => import("../components/Form"));
+const FormList = React.lazy(() => import('../components/FormList'));
+
 export default function AppRouter() { 
 
     const routes = {
-        "/" : () => <Home />,
+        "/" : () => <React.Suspense fallback={<div>Loading...</div>}><Home /></React.Suspense>,
         "/logout": () => <Logout />,
         "/login" : () => <Login />,
-        "/forms" : () => <FormList />,
-        "/forms/:id" : ({ id } : { id : string}) => <Form id={Number(id)} />,
+        "/forms" : () => <React.Suspense fallback={<div>Loading...</div>}><FormList /></React.Suspense>,
+        "/forms/:id" : ({ id } : { id : string}) => <React.Suspense fallback={<div>Loading...</div>}><Form id={Number(id)} /></React.Suspense>,
         //"/preview/:id" : ({ id } : { id : string}) => <Preview id={Number(id)}/>
     }
 
